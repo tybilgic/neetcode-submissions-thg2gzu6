842f490f736915b2b7933f1baadf6a5e543cab1f
Turgut Bilgiç <7927156+tybilgic@users.noreply.github.com>
2026-04-18 18:27:13 +0200

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size())
+        {
+            return false;
+        }
+
+        unordered_map<char, int> charMap;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            char charS = s[i];
+            char charT = t[i];
+            charMap[charS] = charMap[charS] + 1;
+            charMap[charT] = charMap[charT] -1;
+        }
+
+        for (const auto& [key, value] : charMap)
+        {
+            if (value > 0)
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
